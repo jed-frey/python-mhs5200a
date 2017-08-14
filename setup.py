241b6c022756cc01341492a2cb284d84f41b0d91
Frey, Jed <jed-frey+win10@noreply.github.com>
2017-08-14 13:11:57 -0400

    Renamed project in setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 # -*- coding: utf-8 -*-
 from setuptools import setup
 
-setup(name='python-mhs5200a',
+setup(name='python-mhs5200',
       version='0.1',
-      description='Python module for controlling inexpensive MHS-5200A signal generators.',
-      url='https://github.com/jed-frey/python-mhs5200a',
+      description='Python module for controlling inexpensive MHS-5200 signal generators.',
+      url='https://github.com/jed-frey/python-mhs5200',
       author='Jed Frey',
       license='BSD',
-      packages=['python-mhs5200a'],
+      packages=['python-mhs5200'],
       zip_safe=False)
