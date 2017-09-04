d5345fdc87af8d659ea05b4385bb63c396552974
Frey, Jed <jed-frey@users.noreply.github.com>
2017-09-04 19:40:35 -0400

    Removed setwave. Removed test-reports.

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 setup(name='mhs5200',
       version='0.1',
       description='Python module for controlling inexpensive MHS-5200 signal generators.',
-      url='https://github.com/jed-frey/python-mhs5200',
+      url='https://github.com/jed-frey/python_mhs5200',
       author='Jed Frey',
       license='BSD',
       packages=['mhs5200'],
